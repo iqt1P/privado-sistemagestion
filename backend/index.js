001ccc54e3b5d0b908a146665d5a704b365b3e37
iqt1p <gerbin6@gmail.com>
2024-10-26 07:19:18 -0600

    Configuración para escuchar el puerto de Render

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const userRoutes = require('./routes/users');
 
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 //app.use(cors());
